9e9f56f072530ee4ea11a80d97d7cc73cb187764
Tóth Viktor <50296138+vitya0717@users.noreply.github.com>
2024-01-19 12:38:55 +0100

    deltatime

--- vizsga/src/system/systems.js
+++ vizsga/src/system/systems.js
@@ -1,25 +1,38 @@
 import { GameLoop } from "react-game-engine";
 
-const MoveBox = (entities, { input }) => {
-    //-- I'm choosing to update the game state (entities) directly for the sake of brevity and simplicity.
-    //-- There's nothing stopping you from treating the game state as immutable and returning a copy..
-    //-- Example: return { ...entities, t.id: { UPDATED COMPONENTS }};
-    //-- That said, it's probably worth considering performance implications in either case.
-  
-    const { payload } = input.find(x => x.name === "onMouseDown") || {};
-  
-  if(GameLoop.onUpdate){
-    console.log("update");
+const MoveBox = (entities, { input, time }) => {
+  //-- I'm choosing to update the game state (entities) directly for the sake of brevity and simplicity.
+  //-- There's nothing stopping you from treating the game state as immutable and returning a copy..
+  //-- Example: return { ...entities, t.id: { UPDATED COMPONENTS }};
+  //-- That said, it's probably worth considering performance implications in either case.
+
+  const { payload } = input.find(x => x.name === "onKeyDown") || {};
+
+  if (payload) {
+    const box1 = entities["box1"];
+    const speed = 1000;
+    const deltaTime = time.delta / 1000;
+
+    switch (payload.key) {
+      case "w":
+        box1.y -= speed * deltaTime;
+        break;
+      case "s":
+        box1.y += speed * deltaTime;
+        break;
+      case "a":
+        box1.x -= speed * deltaTime;
+        break;
+      case "d":
+        box1.x += speed * deltaTime;
+        break;
+
+      default:
+        break;
+    }
   }
 
-    if (payload) {
-      const box1 = entities["box1"];
-      
-      box1.x = payload.pageX;
-      box1.y = payload.pageY;
-    }
-  
-    return entities;
-  };
-  
-  export { MoveBox };
+  return entities;
+};
+
+export { MoveBox };
