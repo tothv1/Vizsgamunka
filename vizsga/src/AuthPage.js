5c7a87df5d26631fd771b2bbc927ffb03adc0569
kutasierzsi <112945070+kutasierzsi@users.noreply.github.com>
2024-02-27 08:43:30 +0100

    centered authpage
    meow

--- vizsga/src/AuthPage.js
+++ vizsga/src/AuthPage.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom'
 
 const AuthPage = () => {
     return (
-        <div className='container'>
+        <div className='container d-flex  
+        align-items-center  
+        justify-content-center  
+        min-vh-100 text-center'>
             <div>
                 <button className='btn btn-primary'>Regisztráció</button><br />
                 <Link to='/login'>Már van felhasználója? Bejelentkezés</Link>
