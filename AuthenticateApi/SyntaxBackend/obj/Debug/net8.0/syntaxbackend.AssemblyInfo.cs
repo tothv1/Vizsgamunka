5354a4ba02df6edd5ef78df3b8b95d27ff648c73
Tóth Viktor <50296138+vitya0717@users.noreply.github.com>
2024-03-04 12:27:07 +0100

    Revert "Admin felület"
    This reverts commit 06d4c00eef516803bfe343631af0389c7cac1e31.

--- AuthenticateApi/SyntaxBackend/obj/Debug/net8.0/syntaxbackend.AssemblyInfo.cs
+++ AuthenticateApi/SyntaxBackend/obj/Debug/net8.0/syntaxbackend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyntaxBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62305d7861303182390fb39b7085d9df49e70426")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48774c65c278e43b7ad944daa03822df2d321793")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyntaxBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyntaxBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
