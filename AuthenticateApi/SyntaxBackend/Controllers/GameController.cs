0cb3d0f04dc63e179be669b8cd05903011c08b23
Tóth Viktor <50296138+vitya0717@users.noreply.github.com>
2024-03-19 12:12:00 +0100

    Update GameController.cs

--- AuthenticateApi/SyntaxBackend/Controllers/GameController.cs
+++ AuthenticateApi/SyntaxBackend/Controllers/GameController.cs
@@ -34,7 +34,7 @@ namespace GameController.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest("Sikertelen lekÈrdezÈs: " + ex.Message);
+                return BadRequest("Sikertelen lek√©rdez√©s: " + ex.Message);
 
             }
         }
@@ -57,13 +57,13 @@ namespace GameController.Controllers
 
                 if (requestedUser == null)
                 {
-                    return NotFound("A kÈrt felhaszn·lÛ nem tal·lhatÛ.");
+                    return NotFound("A k√©rt felhaszn√°l√≥ nem tal√°lhat√≥.");
                 }
                 return Ok(requestedUser);
             }
             catch (Exception ex)
             {
-                return BadRequest(("Sikertelen lekÈrdezÈs: {0}", ex.Message));
+                return BadRequest(("Sikertelen lek√©rdez√©s: {0}", ex.Message));
 
             }
         }
@@ -80,19 +80,19 @@ namespace GameController.Controllers
 
                 if (requestedUser == null)
                 {
-                    return NotFound("A keresett felhaszn·lÛ nem lÈtezik!");
+                    return NotFound("A keresett felhaszn√°l√≥ nem l√©tezik!");
                 }
                 var requestedStats = context.Userstats.FirstOrDefault(s => s.UserStatId == requestedUser!.UserStatsId);
 
                 if (requestedUser == null)
                 {
-                    return Ok("A kÈrt felhaszn·lÛ nem tal·lhatÛ.");
+                    return Ok("A k√©rt felhaszn√°l√≥ nem tal√°lhat√≥.");
                 }
                 return Ok(requestedStats);
             }
             catch (Exception ex)
             {
-                return BadRequest(("Sikertelen lekÈrdezÈs: {0}", ex.Message));
+                return BadRequest(("Sikertelen lek√©rdez√©s: {0}", ex.Message));
 
             }
         }
@@ -119,7 +119,7 @@ namespace GameController.Controllers
                 await context.Users.AddAsync(user);
                 await context.SaveChangesAsync();
 
-                return Ok("Felhaszn·lÛ lÈtrehozva.");
+                return Ok("Felhaszn√°l√≥ l√©trehozva.");
             }
             catch (Exception ex)
             {
@@ -141,7 +141,7 @@ namespace GameController.Controllers
 
                 if (requestedUser == null)
                 {
-                    return NotFound("A kÈrt felhaszn·lÛ nem tal·lhatÛ!");
+                    return NotFound("A k√©rt felhaszn√°l√≥ nem tal√°lhat√≥!");
                 }
 
                 var requestedStat = context.Userstats.FirstOrDefault(s => s.UserStatId == requestedUser.UserStatsId);
@@ -152,7 +152,7 @@ namespace GameController.Controllers
 
                 await context.SaveChangesAsync();
 
-                return Ok("Sikeresen resetelted a fiÛkodat!");
+                return Ok("Sikeresen resetelted a fi√≥kodat!");
             }
             catch (Exception ex)
             {
@@ -174,7 +174,7 @@ namespace GameController.Controllers
 
                 if (requestedStat == null)
                 {
-                    return NotFound("A kÈrt statisztika nem tal·lhatÛ!");
+                    return NotFound("A k√©rt statisztika nem tal√°lhat√≥!");
                 }
 
                 requestedStat!.Kills += userstat.Kills;
@@ -187,7 +187,7 @@ namespace GameController.Controllers
                 context.Update(requestedStat);
                 await context.SaveChangesAsync();
 
-                return Ok("Friss¸ltek a statisztik·id!");
+                return Ok("Friss√ºltek a statisztik√°id!");
             }
             catch (Exception ex)
             {
@@ -195,6 +195,41 @@ namespace GameController.Controllers
                 throw;
             }
         }
+
+        [Authorize(Roles = "Admin")]
+[HttpPut("adminUpdateAccountStats")]
+public async Task<ActionResult> AdminUpdateUserStat([FromBody] Userstat userstat)
+{
+    try
+    {
+
+        var context = new GameContext();
+
+        var requestedStat = context.Userstats.FirstOrDefault(u => u.UserStatId == userstat.UserStatId);
+
+        if (requestedStat == null)
+        {
+            return NotFound("A k√©rt statisztika nem tal√°lhat√≥!");
+        }
+
+        requestedStat!.Kills = userstat.Kills;
+        requestedStat.HighestLevel = userstat.HighestLevel;
+        requestedStat.HighestKillCount = userstat.HighestKillCount;
+
+        requestedStat!.Deaths = userstat.Deaths;
+        requestedStat!.Timesplayed = userstat.Timesplayed;
+
+        context.Update(requestedStat);
+        await context.SaveChangesAsync();
+
+        return Ok("Sikeresen friss√≠tetted a statisztik√°kat!");
+    }
+    catch (Exception ex)
+    {
+        return BadRequest(ex.Message);
+        throw;
+    }
+}
 
 
 
