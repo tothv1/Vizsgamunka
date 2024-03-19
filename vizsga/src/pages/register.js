7e2a9a64112945bf10606326ca44435ee612f928
Laff <gerreger0170@gmail.com>
2024-03-19 14:03:32 +0100

    register fix
    there was a typo

--- vizsga/src/pages/register.js
+++ vizsga/src/pages/register.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import menuhatter from '../Assets/background/menuhatter.jpg'
 import axios from 'axios'
-import {jwtDecode} from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode'
 
 const Register = () => {
   const navigate = useNavigate();
-  
+
   return (
     <div className='container w-25'>
       <form onSubmit={async (e) => {
@@ -18,36 +18,36 @@ const Register = () => {
           fullname: e.target.fullname.value,
           email: e.target.email.value,
           password: e.target.password.value,
-          passwordRepeat: e.target.passwordre.value,
+          passwordRepeate: e.target.passwordre.value,
         })
-        .then(async (response) => {
-          if (response.data.status == 200) {
-            console.log(response.data);
-          }
-        });
+          .then(async (response) => {
+            if (response.data.status == 200) {
+              console.log(response.data);
+            }
+          });
       }} className='form-control'>
         <div>
           <img className='w-100' src={menuhatter} alt='logo' />
         </div>
         <div className='form-group'>
           <label>Felhasználónév</label>
-          <input id='username' type='text' className='form-control' required/>
+          <input id='username' type='text' className='form-control' required />
         </div>
         <div className='form-group'>
           <label>Teljes név</label>
-          <input id='fullname' type='text' className='form-control' required/>
+          <input id='fullname' type='text' className='form-control' required />
         </div>
         <div className='form-group'>
           <label>E-mail cím</label>
-          <input id='email' type='email' className='form-control' required/>
+          <input id='email' type='email' className='form-control' required />
         </div>
         <div className='form-group'>
           <label>Jelszó</label>
-          <input id='password' type='password' className='form-control' required/>
+          <input id='password' type='password' className='form-control' required />
         </div>
         <div className='form-group'>
           <label>Jelszó megerősítés</label>
-          <input id='passwordre' type='password' className='form-control' required/>
+          <input id='passwordre' type='password' className='form-control' required />
         </div>
         <button type='submit' className='btn btn-primary mt-2'>Regisztráció</button>
       </form>
