b118d1e7486055614970f739342cdc3e70693374
kutasierzsi <112945070+kutasierzsi@users.noreply.github.com>
2024-04-23 13:07:09 +0200

    icerod

--- vizsga/src/system/PassiveItems/CardPool.js
+++ vizsga/src/system/PassiveItems/CardPool.js
@@ -2,13 +2,15 @@ import { BaseCritCardSelectable } from "./CritItem";
 import { BaseDMGCardSelectable } from "./BaseDMGStat";
 import { BaseSpeedCardSelectable } from "./BaseSpeedStat";
 import { BowCardSelectable } from "../Weapons/BowCard";
+import { IceRodCardSelectable } from "../Weapons/IcerodCard";
 
 class CardPool{
     card =[
     new BowCardSelectable(),
     new BaseCritCardSelectable(),
     new BaseDMGCardSelectable(),
-    new BaseSpeedCardSelectable()];
+    new BaseSpeedCardSelectable(),
+   ];
 }
 
 
