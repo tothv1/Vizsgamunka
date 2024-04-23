99883c101e5c2102b13915e4ec65388bc940d5c4
kutasierzsi <112945070+kutasierzsi@users.noreply.github.com>
2024-04-23 12:00:16 +0200

    bowcardselectable

--- vizsga/src/system/PassiveItems/CardPool.js
+++ vizsga/src/system/PassiveItems/CardPool.js
@@ -1,6 +1,7 @@
 import { BaseCritCardSelectable } from "./CritItem";
 import { BaseDMGCardSelectable } from "./BaseDMGStat";
 import { BaseSpeedCardSelectable } from "./BaseSpeedStat";
+import { BowCardSelectable } from "../Weapons/BowCard";
 
 class CardPool{
     card =[
